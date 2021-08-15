fc64853b6ef2b54fd2c06474793f6c8ec8d16019
Jair Pereira Junior <pereira-junior.ua.ws@alumni.tsukuba.ac.jp>
2021-08-15 22:44:47 +0900

    add ata ffa

---
+++ metaheuristic/ata.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+def ata(problem, maxnfe, n, tolerance, w, seed, file):
+    myrng = np.random.RandomState(seed)
+
+    ## initialization ##
+    Px = np.array([myrng.uniform(problem.lower_bounds, problem.upper_bounds, problem.dimension) for _ in range(n)])
+
+    ## evalute ##
+    Pf = np.array([problem(x) for x in Px])
+    nfe = len(Pf)
+
+    ## find gbest ##
+    current_best = np.argmin(Pf)
+    gbest_x = Px[current_best].copy()
+    gbest_f = Pf[current_best]
+
+    ## find pbest ##
+    Pbest_x = np.array([x.copy() for x in Px])
+    Pbest_f = Pf[:]
+    
+    ## history ##
+    str_gbestx = ";".join(map(str, gbest_x))
+    file.write(f"{nfe},{gbest_f},{str_gbestx}\n")
+
+    gbest_f_prev = gbest_f_cur = gbest_f
+    while nfe <= maxnfe:
+        if gbest_f_prev-gbest_f_cur < tolerance: #migration
+            for i in range(n):
+                r1 = myrng.uniform(0, 1, problem.dimension)
+                r2 = myrng.uniform(0, 1, problem.dimension)
+                Px[i] = Px[i] + r1*(Pbest_x[i]-Px[i]) + w*r2*(gbest_x-Px[i])
+        else: #propagation
+            for i in range(n):
+                r1 = myrng.uniform(0, 1, problem.dimension)
+                r2 = myrng.uniform(0, 1, problem.dimension)
+                Px[i] = r1*Px[i] + (1-r2)*Px[myrng.randint(0, n)]
+        
+        ## evaluate ##
+        Pf = np.array([problem(x) for x in Px])
+        nfe += len(Pf)
+
+        ## find gbest ##
+        current_best = np.argmin(Pf)
+        if gbest_f >= Pf[current_best]:
+            gbest_x = Px[current_best].copy()
+            gbest_f = Pf[current_best]
+        gbest_f_prev = gbest_f_cur
+        gbest_f_cur  = gbest_f
+
+        ## find pbest ##
+        for i in range(n):
+            if Pbest_f[i] >= Pf[i]:
+                Pbest_x[i] = Px[i].copy()
+                Pbest_f[i] = Pf[i]
+                
+        ## history ##
+        str_gbestx = ";".join(map(str, gbest_x))
+        file.write(f"{nfe},{gbest_f},{str_gbestx}\n")
+    
+    result = {"x"  :gbest_x,
+              "f"  :gbest_f,
+              "nfe":nfe}
+    
+    return result
---
+++ metaheuristic/ffa.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+def ffa(problem, maxnfe, n, alpha, gamma, seed, file):
+    myrng = np.random.RandomState(seed)
+
+    ## initialization ##
+    Px = np.array([myrng.uniform(problem.lower_bounds, problem.upper_bounds, problem.dimension) for _ in range(n)])
+
+    ## evalute ##
+    Pf = np.array([problem(x) for x in Px])
+    nfe = len(Pf)
+
+    ## find gbest ##
+    current_best = np.argmin(Pf)
+    gbest_x = Px[current_best].copy()
+    gbest_f = Pf[current_best]
+    
+    ## history ##
+    # str_gbestx = ";".join(map(str, gbest_x))
+    # file.write(f"{nfe},{gbest_f},{str_gbestx}\n")
+    
+    while nfe <= maxnfe:# and not problem.final_target_hit:
+        i_sorted = np.argsort(-Pf) #best at the end
+        for k, i in enumerate(i_sorted):
+            for j in i_sorted[k+1:]:
+                rnd = myrng.uniform(-0.5, +0.5, problem.dimension)
+                if Pf[i] > Pf[j]:
+                    dist = np.sqrt((Px[j]-Px[i])**2)
+                    beta = 0.2 + (1-0.2)*np.exp(-gamma*dist*dist)
+                    # attraction + random walk
+                    Px[i] = Px[i] + beta*(Px[j]-Px[i]) + (alpha/100)*rnd
+                else:
+                    # random walk
+                    Px[i] = Px[i] + alpha*rnd
+        
+        ## best particle random walk ##
+        Px[current_best] += (alpha/1000)*myrng.uniform(-0.01, +0.01, problem.dimension)
+        
+        ## repair ##
+        Px = [np.clip(x, problem.lower_bounds, problem.upper_bounds) for x in Px]
+        
+        ## evalute ##
+        Pf = np.array([problem(x) for x in Px])
+        nfe += len(Pf)
+
+        ## update gbest ##
+        current_best = np.argmin(Pf)
+        if gbest_f >= Pf[current_best]:
+            gbest_x = Px[current_best].copy()
+            gbest_f = Pf[current_best]
+            
+        ## history ##
+        # str_gbestx = ";".join(map(str, gbest_x))
+        # file.write(f"{nfe},{gbest_f},{str_gbestx}\n")
+
+    result = {"x"  :gbest_x,
+              "f"  :gbest_f,
+              "nfe":nfe}
+              
+    return result

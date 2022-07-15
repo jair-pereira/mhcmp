301cf46d34513bbd86288643be5f230e4ac8adca
Jair Pereira Junior <pereira-junior.ua.ws@alumni.tsukuba.ac.jp>
2022-07-15 11:54:11 +0900

    Update gsa.py
    + kbest method

--- metaheuristic/gsa.py
+++ metaheuristic/gsa.py
@@ -11,7 +11,7 @@ def gsa(problem, maxnfe, n, g0, alpha, seed, file=None):
     Pmass  = np.zeros(n) # mass (Mi=Ma=Mp)
     Pmi    = np.zeros(n) # aux for mass
     Pforce = np.zeros((n, problem.dimension))
-    Pacceleration = np.zeros(n) 
+    Pacceleration = np.zeros(n)
 
     ## evalute ##
     Pf = np.array([problem(x) for x in Px])
@@ -22,33 +22,36 @@ def gsa(problem, maxnfe, n, g0, alpha, seed, file=None):
     gbest_x = Px[current_best].copy()
     gbest_f = Pf[current_best]
     gbest_fi = gbest_f #at iteration i
-    
+
     current_worst = np.argmax(Pf)
     gworst_fi = Pf[current_worst]
-    
+
     ## history ##
     if file:
         str_gbestx = ";".join(map(str, gbest_x))
         file.write(f"{nfe},{gbest_f},{str_gbestx}\n")
-    
+
     max_iteration = np.ceil(maxnfe/n)
-    kbest = list(map(int, np.linspace(n, 1, int(max_iteration))))
-    
+
+    ## kbest ##
+    kbest1 = np.linspace(n, 1, int(max_iteration)) # linear, slow
+    kbest2 = [int(np.ceil(n*np.e**(-alpha*i/max_iteration))) for i in range(max_iteration)] #exp fast
+    kbest3 = map(int, [(k1+k2)/2 for k1, k2 in zip(kbest1, kbest2)]) # exp+linear
+
     iteration=0
     while nfe+n <= maxnfe and not problem.final_target_hit:
         # compute g
         g = g0*np.e**(-alpha*iteration/max_iteration)
-        
+
         # compute mass
         Pmi = (Pf-gworst_fi)/(np.finfo('float').eps+gbest_fi-gworst_fi)
         Pmass = Pmi/sum(Pmi)
         Pmass.resize(n,1)
-        
+
         #choose k
-        #K = np.arange(0, n)
-        nk = int(np.ceil(n*np.e**(-alpha*iteration/max_iteration)))
+        nk = int(kbest3[iteration])
         K = np.argpartition(Pf, -nk)[:nk]
-        
+
         # compute force
         for i in range(n):
             Pforce[i] = 0
@@ -58,35 +61,36 @@ def gsa(problem, maxnfe, n, g0, alpha, seed, file=None):
                 c = (Px[k]-Px[i])
 
                 Pforce[i] += c*(a/b) * myrng.uniform(0,1,problem.dimension)
-        
+
         # update acceleration, velocity, and position
         Pacceleration = Pforce/(Pmass+np.finfo('float').eps)
-        Pv = [Pacceleration[i] + Pv[i]*myrng.uniform(0,1,problem.dimension) for i in range(n)]
+        R  = myrng.random((n, problem.dimension))
+        Pv = [Pacceleration[i] + Pv[i]*R[i] for i in range(n)]
         Px = np.array([np.clip(Px[i]+Pv[i], problem.lower_bounds, problem.upper_bounds) for i in range(n)])
 
         ## evaluate ##
         Pf = np.array([problem(x) for x in Px])
         nfe += len(Pf)
-        
+
         ## update gbest and gworst ##
         current_best = np.argmin(Pf)
         if gbest_f >= Pf[current_best]:
             gbest_x = Px[current_best].copy()
             gbest_f = Pf[current_best]
         gbest_fi = Pf[current_best]
-            
+
         current_worst = np.argmax(Pf)
         gworst_fi = Pf[current_worst]
-        
+
         ## history ##
         if file:
             str_gbestx = ";".join(map(str, gbest_x))
             file.write(f"{nfe},{gbest_f},{str_gbestx}\n")
-        
+
         iteration+=1
-    
+
     result = {"x"  :gbest_x,
               "f"  :gbest_f,
               "nfe":nfe}
-    
+
     return result
